b95bd91daeaee4c7d8692c04f442986b4e5a0ee8
ShreeyansB <sbahadkar@rocketmail.com>
2021-09-29 16:17:49 +0530

    Setup env, init bot and added commands

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/shreeyansb/echgobot
+
+go 1.17
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	// .env load
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	kBotToken := os.Getenv("BOT_TOKEN")
+	kGroupID, _ := strconv.ParseInt(os.Getenv("GROUP_ID"), 10, 64)
+
+	// bot init
+	bot, err := tgbotapi.NewBotAPI(kBotToken)
+	if err != nil {
+		log.Fatal("Err: ", err)
+	}
+
+	// bot.Debug = true
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://1b22-110-224-131-152.ngrok.io"))
+	if err != nil {
+		log.Fatal("Err: ", err)
+	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal("Err: ", err)
+	}
+	fmt.Println(info.URL)
+	updates := bot.ListenForWebhook("/")
+
+	go http.ListenAndServe(":8080", nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update.Message.Text)
+
+		if update.Message.IsCommand() {
+			isValidComm := true
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			if update.Message.Chat.ID != kGroupID {
+				// if false {
+				msg.Text = "`[!] Bot Not Authorised`"
+				msg.ParseMode = "MarkdownV2"
+				bot.Send(msg)
+				continue
+			}
+
+			switch update.Message.Command() {
+			case "start":
+				msg.Text = "Use this bot to echo some shit.\n/start - Info\n/echo - Echo stuff normally\n/echob - Echo stuff in bold\n/echoi - Echo stuff in italic\n/echou - Echo stuff underlined\n/echom - Echo stuff in monospace\n/echo(b/i/u) - Echo stuff in combinations of b/i/u, order alphabetically"
+			case "echo":
+				msg.Text = update.Message.CommandArguments()
+			case "echob":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "*" + update.Message.CommandArguments() + "*"
+			case "echoi":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "_" + update.Message.CommandArguments() + "_"
+			case "echou":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "__" + update.Message.CommandArguments() + "__"
+			case "echobi":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "*_" + update.Message.CommandArguments() + "_*"
+			case "echobu":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "*__" + update.Message.CommandArguments() + "__*"
+			case "echoiu":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "_ __" + update.Message.CommandArguments() + "__ _"
+			case "echobiu":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "*_ __" + update.Message.CommandArguments() + "__ _*"
+			case "echom":
+				msg.ParseMode = "MarkdownV2"
+				msg.Text = "`" + update.Message.CommandArguments() + "`"
+			default:
+				isValidComm = false
+			}
+			if isValidComm {
+				bot.Send(msg)
+			}
+		}
+	}
+}
